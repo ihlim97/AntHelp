0a4432717bb06f0f780579c0386247caa766d57a
Ie Howe <iehowe97@gmail.com>
2018-10-23 21:18:17 +0800

    updated services section

--- AntHELPApp/resources/views/components/navbar.blade.php
+++ AntHELPApp/resources/views/components/navbar.blade.php
@@ -9,10 +9,10 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link active" href="index.html"><b>Home <span class="sr-only">(current)</span></b></a>
+                <a class="nav-link active" href="/"><b>Home <span class="sr-only">(current)</span></b></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="search.html"><b>Services</b></a>
+                <a class="nav-link" href="services"><b>Services</b></a>
             </li>
         </ul>
         <a href="senior-signup-page.html" class="ml-auto btn btn-outline-light mr-3">SIGNUP</a>
--- AntHELPApp/resources/views/services/index.blade.php
+++ AntHELPApp/resources/views/services/index.blade.php
@@ -1,39 +1,51 @@
 @extends('layouts.app', ['showCurator' => true])
 
+@section('header')
+    @component('components.banner', ['imagePath'=> 'img/banners/handhold.jpg', 'bannerType'=> 'banner-medium'])
+        @slot('serviceCurator')
+            @component('components.service-curator', ['title' => 'Refine your search'])                              
+            @endcomponent
+        @endslot
+    @endcomponent
+@endsection
+
 @section('content')
 
-    @if (count($services) > 0)
-
-        <div class="row">
-            <div class="col-12">
-                <p>Yay! We found {{count($services)}} service(s)</p>
-            </div>
-            <div class="col-12 cards-container">
-
-                <div class="row">
-                    @foreach ($services as $service)
-                        <div class="col-12 col-lg-4">
-                            @component("components.servicecard", [
-                                'freshness' => 'NEW!',
-                                'category' => $service->category,
-                                'rating' => '5.0',
-                                'description' => $service->description,
-                                'rate' => $service->rate,
-                                'rate_type' => $service->rate_type,
-                                'link' => "services/".($service->id)
-                            ])         
-                            @endcomponent
-                        </div>
-                        {{$services->links()}}
-                    @endforeach
+    <div class="container">
+        @if (count($services) > 0)
+            <div class="row">
+                <div class="col-12">
+                    <p>Yay! We found {{count($services)}} service(s)</p>
                 </div>
-
+                <div class="col-12 cards-container">
+                    <div class="row">
+                        @foreach ($services as $service)
+                            <div class="col-12 col-lg-4">
+                                @component("components.servicecard", [
+                                    'freshness' => 'NEW!',
+                                    'category' => $service->category,
+                                    'rating' => '5.0',
+                                    'description' => $service->description,
+                                    'rate' => $service->rate,
+                                    'rate_type' => $service->rate_type,
+                                    'link' => "services/".($service->id)
+                                ])         
+                                @endcomponent
+                            </div>
+                            {{$services->links()}}
+                        @endforeach
+                    </div>
+                </div>
             </div>
-        </div>
+        @else
+            <h4 class="text-muted">No Services Yet!</h4>
+        @endif
+    </div>
+
+@endsection
+
+@section('footer')
+    @component('components.footer')
         
-
-    @else
-        <h4 class="text-muted">No Services Yet!</h4>
-    @endif
-
+    @endcomponent
 @endsection
