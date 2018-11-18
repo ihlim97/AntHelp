40666366f5a156bcdb507385b615ae31c24ad107
YinSye <jijingazat@gmail.com>
2018-11-19 00:25:50 +0800

    Updates Submit Request php

--- src/search.php
+++ src/search.php
@@ -1,8 +1,7 @@
 <?php 
 session_start();
-include("header.php");
+include("header.php"); 
 ?>
-
         <div class="banner banner-medium d-flex align-items-center" style="background-image: url('assets/img/banners/handhold.jpg')">
             <div class="wrapper">
                 <div class="container">
@@ -74,62 +73,6 @@ include("header.php");
             </div>
 
             <div class="col-12 cards-container">
-                
-                <div class="card service-card mb-3">
-                    <div class="row no-gutters">
-                        <div class="col-12 col-lg-4">
-                            <img src="https://source.unsplash.com/500x400/?maid" alt="" class="card-img">
-                        </div>
-                        <div class="col">
-                            <div class="card-body">
-                                <h6 class="card-subtitle text-info">NEW!</h6>
-                                <h3 class="card-title text-dark"><b>Home Cleaning Service</b></h3>
-                                <a data-toggle="modal" data-target="#request-confirmation" class="btn btn-lg btn-success cta text-white d-none d-sm-block">Book Now</a>
-                                <div class="rating text-warning">
-                                    <span class="fas fa-star"></span>
-                                    <span class="fas fa-star"></span>
-                                    <span class="fas fa-star"></span>
-                                    <span class="fas fa-star"></span>
-                                    <span class="fas fa-star"></span>
-                                    5.0
-                                </div>
-                            </div>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item">
-                                    <div class="row no-gutters">
-                                        <div class="col-12 col-sm-8">
-                                            <ul class="disc text-dark">
-                                                <li>Supreme cleaning services by trained staff</li>
-                                                <li>Extra careful with your belongings</li>
-                                                <li>Cleaning supplies can be arranged (if required)</li>
-                                                <li>FREE! Essential oils for 1 room</li>
-                                            </ul>
-                
-                                            <div class="row d-sm-none">
-                                                <div class="col text-left">
-                                                    <a data-toggle="modal" data-target="#request-confirmation" class="btn btn-lg btn-success mt-3 text-white">Book Now</a>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="pricing text-right">
-                                                        <h1 class="text-danger m-0">RM120</h1>
-                                                        <h6 class="text-dark">per hour</h6>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col d-none d-sm-block">
-                                            <div class="pricing text-right">
-                                                <h1 class="text-danger m-0">RM120</h1>
-                                                <h6 class="text-dark">per hour</h6>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
                 <div class="card service-card mb-3">
                     <div class="row no-gutters">
                         <div class="col-12 col-lg-4">
@@ -184,6 +127,71 @@ include("header.php");
                         </div>
                     </div>
                 </div>
+
+    <?php 
+    include("config.php");
+    $sql = "SELECT * FROM services";
+    $services = mysqli_query($con, $sql);
+    $count=0;
+    if (mysqli_num_rows($services) > 0) {
+        while($row = mysqli_fetch_assoc($services)) {
+        $row['service_type'];
+        echo '
+            <div class="card service-card mb-3 id="'.$count.'">
+                <div class="row no-gutters">
+                    <div class="col-12 col-lg-4">
+                        <img src="https://source.unsplash.com/500x400/?maid" class="card-img">
+                    </div>
+                    <div class="col">
+                        <div class="card-body">
+                            <h6 class="card-subtitle text-info">NEW!</h6>
+                            <h3 class="card-title text-dark"><b>'.$row['service_type'].'</b></h3>
+                            <a href=service-request.php?type='.$row['service_type'].'?id='.$row['service_id'].' class="btn btn-lg btn-success cta text-white d-none d-sm-block">Book Now</a>
+                            <div class="rating text-warning">
+                                <span class="fas fa-star"></span>
+                                <span class="fas fa-star"></span>
+                                <span class="fas fa-star"></span>
+                                <span class="fas fa-star"></span>
+                                <span class="fas fa-star"></span>
+                                5.0
+                            </div>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <div class="row no-gutters">
+                                    <div class="col-12 col-sm-8">
+                                        <ul class="disc text-dark">
+                                            <li>'.$row['service_description'].'</li>
+                                        </ul>
+                
+                                        <div class="row d-sm-none">
+                                            <div class="col text-left">
+                                                <a href=service-request.php?id='.$row['service_id'].' class="btn btn-lg btn-success mt-3 text-white">Book Now</a>
+                                            </div>
+                                            <div class="col">
+                                                <div class="pricing text-right">
+                                                    <h1 class="text-danger m-0">RM'.$row['service_rate'].'</h1>
+                                                    <h6 class="text-dark">per hour</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col d-none d-sm-block">
+                                        <div class="pricing text-right">
+                                            <h1 class="text-danger m-0">RM'.$row['service_rate'].'</h1>
+                                            <h6 class="text-dark">per hour</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>';
+            $count = $count + 1;
+        }
+    }
+?>
 
             </div>
         </div>
