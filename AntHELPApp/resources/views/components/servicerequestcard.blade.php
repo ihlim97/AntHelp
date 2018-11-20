0b40ecf762b967ca7cdb261664a990f1fda9d471
Ie Howe <iehowe97@gmail.com>
2018-11-20 14:37:35 +0800

    Added descriptive classes to ensure testability

--- AntHELPApp/resources/views/components/servicerequestcard.blade.php
+++ AntHELPApp/resources/views/components/servicerequestcard.blade.php
@@ -1,24 +1,24 @@
-<div class="card service-card-2 mb-3">
+<div class="card service-card-2 mb-3" data-service-request-id="{{$svc_request->id}}">
     <div class="row no-gutters">
         <div class="col-12 col-lg-4">
             <div class="card-body pb-0">
                 @if($svc_request->status == 'PENDING')
-                    <div class="badge badge-warning">{{$svc_request->status}}</div>
+                    <div class="badge badge-warning req-status">{{$svc_request->status}}</div>
                 @endif
                 @if($svc_request->status == 'COMPLETED')
-                    <div class="badge badge-success">{{$svc_request->status}}</div>
+                    <div class="badge badge-success req-status">{{$svc_request->status}}</div>
                 @endif
                 @if($svc_request->status == 'ACCEPTED')
-                    <div class="badge badge-info">{{$svc_request->status}}</div>
+                    <div class="badge badge-info req-status">{{$svc_request->status}}</div>
                 @endif
                 @if($svc_request->status == 'DECLINED' || $svc_request->status == 'CANCELLED')
-                    <div class="badge badge-danger">{{$svc_request->status}}</div>
+                    <div class="badge badge-danger req-status">{{$svc_request->status}}</div>
                 @endif
                 @if($svc_request->status == 'EXPIRED')
-                    <div class="badge badge-secondary">{{$svc_request->status}}</div>
+                    <div class="badge badge-secondary req-status">{{$svc_request->status}}</div>
                 @endif
-                <h3 class="card-title text-dark">{{App\Service::find($svc_request->service_id)->category}}</h3>
-                <p class="card-text">{{App\Service::find($svc_request->service_id)->description}}</p>
+                <h3 class="card-title text-dark req-svc-category">{{App\Service::find($svc_request->service_id)->category}}</h3>
+                <p class="card-text req-svc-desc">{{App\Service::find($svc_request->service_id)->description}}</p>
             </div>
         </div>
         <div class="col-12 col-lg-8">
@@ -26,39 +26,39 @@
                 <div class="col-12 col-sm-9">
                     <div class="row mt-4 mb-2 metadata-wrapper">
                         <div class="col-4">
-                            <div class="metadata">
+                            <div class="metadata req-requestor">
                                 <p class="card-subtitle text-muted">Requested By</p>
                                 <h5 class="card-title text-dark">{{ App\User::find($svc_request->user_id)->name}}</h5>
                             </div>
                         </div>
-                        <div class="col-4">
+                        <div class="col-4 req-start">
                             <div class="metadata">
                                 <p class="card-subtitle text-muted">Start Date</p>
                                 <h5 class="card-title text-dark">{{Carbon\Carbon::parse($svc_request->start_date_time)->format('M j, H:i A')}}</h5>
                             </div>
                         </div>
                         <div class="col-4">
-                            <div class="metadata">
+                            <div class="metadata req-end">
                                 <p class="card-subtitle text-muted">End Date</p>
                                 <h5 class="card-title text-dark">{{Carbon\Carbon::parse($svc_request->end_date_time)->format('M j, H:i A')}}</h5>
                             </div>
                         </div>
                         <div class="col-4">
-                            <div class="metadata">
+                            <div class="metadata req-duration">
                                 <p class="card-subtitle text-muted">Duration</p>
                                 <h5 class="card-title text-dark">{{$svc_request->duration}}</h5>
                             </div>
                         </div>
-                        <div class="col-4">
+                        <div class="col-4 req-worth">
                             <div class="metadata">
                                 <p class="card-subtitle text-muted">Worth</p>
                                 <h5 class="card-title text-success">RM {{$svc_request->total}}</h5>
                             </div>
                         </div>
                         <div class="col-4">
-                            <div class="metadata">
+                            <div class="metadata req-id">
                                 <p class="card-subtitle text-muted">Request ID</p>
-                                <h5 class="card-title text-success"># {{$svc_request->id}}</h5>
+                                <h5 class="card-title"># {{$svc_request->id}}</h5>
                             </div>
                         </div>
                     </div>
